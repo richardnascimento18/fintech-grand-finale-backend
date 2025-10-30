bd1bcf0c7e353c0d37667c9e2938ca8f6e16590b
Richard Nascimento <computer.richard0923@gmail.com>
2025-10-30 09:12:23 -0300

    chore(app): push first commit
    - Uploads the first commit to the repository
    
    Closes TRELLO-(#QoznmdVD/17)

---
+++ src/main/java/br/com/fiap/cashiq_backend/CashiqBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.cashiq_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashiqBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CashiqBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/cashiq_backend/CashiqBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.cashiq_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashiqBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
